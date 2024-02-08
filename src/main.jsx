f2e1485a9119b18e8c1f017612ea0ac6f32ff6b3
Tanaka Chidemo <98038588+TanakaChidemo@users.noreply.github.com>
2024-02-08 09:10:55 +0400

    Update main.jsx
    Appended Contact component as a child of Root

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import ErrorPage from "./error-page.jsx";
+import Contact from "./routes/contact";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <Error/>
-  },
+    errorElement: <ErrorPage/>,
+  
+children: [
+{
+    path: "contacts/:contactId",
+    element: <Contact/>,
+},
+],
+},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
