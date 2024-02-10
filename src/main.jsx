ff1d655a5ab90d3af95e2b377258ca9b2dbd4882
Tanaka Chidemo <chidemo@icloud.com>
2024-02-10 17:42:57 +0400

    added Outlet and Link to root component

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import Contact from "./routes/contact";
 import "./index.css";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <Error/>
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:id",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
